d3ac8aba64e0f6785aa820f8cacafcd60e38c6f7
inVariabl <danonguitar@gmail.com>
2020-02-15 13:58:21 -0800

    Singular Files + More Verbose

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 console.log("Starting ADA Font Check...");
-var input = document.querySelector('input[type="file"]');
-input.addEventListener('change', onReadFile, false);
+var folder = document.querySelector('input[name="folder"]');
+var file = document.querySelector('input[name="file"]');
+folder.addEventListener('change', onReadFolder, false);
+file.addEventListener('change', onReadFile, false);
 
-function onReadFile(e) {
+function onReadFolder(e) {
   var fontFiles = e.target.files;
   for (var i = 0; i < fontFiles.length; i++) {
     var file = fontFiles[i];
@@ -12,21 +14,7 @@ function onReadFile(e) {
         var font = opentype.parse(e.target.result);
         var result = checkfont(font);
 
-        function adaresult() {
-          if (result.test.ada) {
-            return ["✔", "pass"];
-          } else if (result.test.ada === false) {
-            if (document.getElementById("checkbox").checked === false) {
-              return ["✘", "fail"];
-            }
-          } else {
-            alert("Error! ADA results error.");
-          }
-        }
-
-        document.getElementById('fonttable').innerHTML += '<tr id="tableresults" class=' + adaresult()[1] + '><td class="row"><a href="#" onclick="moreFontInfo(result)">' + " " + result.name.en + '</a></td><td class="row">' + adaresult()[0] + '</td></tr>';
-
-        //displayResults();
+        document.getElementById('fonttable').innerHTML += '<tr id="tableresults" class=' + adaresult(result.test.ada)[1] + '><td class="row"><a href="#" onclick="moreFontInfo(result)">' + " " + result.name.en + '</a></td><td class="row">' + adaresult(result.test.ada)[0] + '</td></tr>';
       } catch (err) {
         console.log("Caught Error!");
         return false;
@@ -37,12 +25,39 @@ function onReadFile(e) {
   }
 }
 
-function adaresult() {
-  if (result.test.ada) {
+function onReadFile(e) {
+  var fontFiles = e.target.files;
+  for (var i = 0; i < fontFiles.length; i++) {
+    var file = fontFiles[i];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      try {
+        var font = opentype.parse(e.target.result);
+        var result = checkfont(font);
+        document.getElementById('fonttable').innerHTML += '<tr id="tableresults" class=' + adaresult(result.test.ada)[1] + '><td class="row">' + result.name.en + '</a></td><td class="row">' + adaresult(result.test.ada)[0] + '</td></tr> <tr><td>Not Italic</td><td>' + result.test.notitalic + '</td></tr><tr><td>Stroke Test</td><td>' + result.test.stroke + '</td></tr><tr><td>Body Test</td><td>' + result.test.body + '</td></tr><tr><td>Letter I Ratio</td><td>' + result.letter_i.ratio + '</td></tr><tr><td>Letter H Ratio</td><td>' + result.letter_h.ratio + '</td></tr><tr><td>Letter O Ratio</td><td>' + result.letter_o.ratio + '</td></tr>';
+
+      } catch (err) {
+        console.log("Caught Error!");
+        return false;
+      }
+    };
+    reader.onerror = function(err) {};
+    reader.readAsArrayBuffer(file);
+  }
+}
+
+function adaresult(result) {
+  if (result) {
     return ["✔", "pass"];
-  } else if (result.test.ada === false) {
-    return ["✘", "fail"];
+  } else if (result === false) {
+    if (document.getElementById("checkbox").checked === false) {
+      return ["✘", "fail"];
+    }
   } else {
     alert("Error! ADA results error.");
   }
+}
+
+function clearFonts() {
+  document.getElementById('fonttable').innerHTML = "<tr class='tablehead'><th>Font Name</th><th>Test Results</th></tr>";
 }
