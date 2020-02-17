c89dd65ef86dadaae60bc6feb1b53d3164887e3c
inVariabl <danonguitar@gmail.com>
2020-02-16 22:08:48 -0800

    Material Theme 2

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ folder.addEventListener('change', onReadFolder, false);
 file.addEventListener('change', onReadFile, false);
 
 function onReadFolder(e) {
+  clearFonts();
   var fontFiles = e.target.files;
   for (var i = 0; i < fontFiles.length; i++) {
     var file = fontFiles[i];
@@ -13,7 +14,7 @@ function onReadFolder(e) {
       try {
         var font = opentype.parse(e.target.result);
         var result = checkfont(font);
-        document.getElementById('fonttable').innerHTML += '<tr id="tableresults" class=' + adaresult(result.test.ada)[1] + '><td class="row">' + " " + result.name.en + '</a></td><td class="row">' + adaresult(result.test.ada)[0] + '</td></tr>';
+        document.getElementById('fonttable').innerHTML += '<tr id="tableresults" class=' + adaresult(result.test.ada)[1] + '><td class="row">' + " <b>" + result.name.en + '</b></a></td><td class="row">' + adaresult(result.test.ada)[0] + '</td></tr>';
       } catch (err) {
         console.log("Caught Error!");
         return false;
@@ -33,7 +34,7 @@ function onReadFile(e) {
       try {
         var font = opentype.parse(e.target.result);
         var result = checkfont(font);
-        document.getElementById('fonttable').innerHTML += '<tr id="tableresults" class=' + adaresult(result.test.ada)[1] + '><td class="row">' + result.name.en + '</a></td><td class="row">' + adaresult(result.test.ada)[0] + '</td></tr> <tr><td>Not Italic</td><td>' + result.test.notitalic + '</td></tr><tr><td>Stroke Test</td><td>' + result.test.stroke + '</td></tr><tr><td>Body Test</td><td>' + result.test.body + '</td></tr><tr><td>Letter I Ratio</td><td>' + result.letter_i.ratio + '</td></tr><tr><td>Letter H Ratio</td><td>' + result.letter_h.ratio + '</td></tr><tr><td>Letter O Ratio</td><td>' + result.letter_o.ratio + '</td></tr>';
+      document.getElementById('fonttable').innerHTML += '<tbody><tr class=' + adaresult(result.test.ada)[1] + ' id="tableresults" ><td class="row">' + result.name.en + '</a></td><td class="row">' + adaresult(result.test.ada)[0] + '</td></tr> <tr><td>Not Italic</td><td>' + result.test.notitalic + '</td></tr><tr><td>Stroke Test</td><td>' + result.test.stroke + '</td></tr><tr><td>Body Test</td><td>' + result.test.body + '</td></tr><tr><td>Letter I Ratio</td><td>' + result.letter_i.ratio + '</td></tr><tr><td>Letter H Ratio</td><td>' + result.letter_h.ratio + '</td></tr><tr><td>Letter O Ratio</td><td>' + result.letter_o.ratio + '</td></tr></tbody>';
       } catch (err) {
         console.log("Caught Error!");
         return false;
@@ -46,10 +47,12 @@ function onReadFile(e) {
 
 function adaresult(result) {
   if (result) {
-    return ["✔", "pass"];
+    //return ["✔", "pass"];
+    return ["<i class='material-icons green-text'>check_circle</i>", '"green lighten-5"'];
   } else if (result === false) {
     if (document.getElementById("checkbox").checked === false) {
-      return ["✘", "fail"];
+      //return ["✘", "fail"];
+    return ["<i class='material-icons red-text'>cancel</i>", ""];
     }
   } else {
     alert("Error! ADA results error.");
@@ -57,5 +60,6 @@ function adaresult(result) {
 }
 
 function clearFonts() {
-  document.getElementById('fonttable').innerHTML = "<tr class='tablehead'><th>Font Name</th><th>Test Results</th></tr>";
+  console.log("Clearing Fonts");
+  document.getElementById('fonttable').innerHTML = " <tr> <th><h5>Font Name</h5></th> <th><h5>Test Result</h5></th> </tr> ";
 }
