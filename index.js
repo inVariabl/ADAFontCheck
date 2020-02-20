5d5ab4f61a6fe5221bcc1617c68652b2586a111b
inVariabl <danonguitar@gmail.com>
2020-02-19 22:34:40 -0800

    Logo + Styling Fixes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (folder) {
   folder.addEventListener('change', onReadFolder, false);
 }
 if (file) {
-  file.addEventListener('change', onReadFile, false);
+  file.addEventListener('change', onReadFolder, false);
 }
 
 function onReadFolder(e) {
@@ -18,28 +18,7 @@ function onReadFolder(e) {
       try {
         var font = opentype.parse(e.target.result);
         var result = checkfont(font);
-        console.log(result);
-        document.getElementById('displayresults').innerHTML += '<li> <div class="collapsible-header row ' + adaresult(result.test.ada)[1] + '"> <div class="col s11"><b>' + result.name.en + '</b></div> <div class="col s1">' + adaresult(result.test.ada)[0] + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s11">Not Italic Test</div> <div class="col s1">' + result.test.notitalic + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Stroke Width Test</div> <div class="col s2">' + result.test.stroke + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Body Width Test</div> <div class="col s2">' + result.test.body + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Letter "I" Ratio</div> <div class="col s2">' + result.letter_i.ratio + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Letter "H" Ratio</div> <div class="col s2">' + result.letter_h.ratio + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Letter "O" Ratio</div> <div class="col s2">' + result.letter_o.ratio + '</div> </div> </li>';
-      } catch (err) {
-        console.log("Caught Error!");
-        return false;
-      }
-    };
-    reader.onerror = function(err) {};
-    reader.readAsArrayBuffer(file);
-  }
-}
-
-function onReadFile(e) {
-  var fontFiles = e.target.files;
-  for (var i = 0; i < fontFiles.length; i++) {
-    var file = fontFiles[i];
-    var reader = new FileReader();
-    reader.onload = function(e) {
-      try {
-        var font = opentype.parse(e.target.result);
-        var result = checkfont(font);
-        document.getElementById('fonttable').innerHTML += '<tbody><tr class=' + adaresult(result.test.ada)[1] + ' id="tableresults" ><td class="row">' + result.name.en + '</a></td><td class="row">' + adaresult(result.test.ada)[0] + '</td></tr> <tr><td>Not Italic</td><td>' + result.test.notitalic + '</td></tr><tr><td>Stroke Test</td><td>' + result.test.stroke + '</td></tr><tr><td>Body Test</td><td>' + result.test.body + '</td></tr><tr><td>Letter I Ratio</td><td>' + result.letter_i.ratio + '</td></tr><tr><td>Letter H Ratio</td><td>' + result.letter_h.ratio + '</td></tr><tr><td>Letter O Ratio</td><td>' + result.letter_o.ratio + '</td></tr></tbody>';
+        document.getElementById('displayresults').innerHTML += '<li><div class="collapsible-header row ' + adaresult(result.test.ada)[1] + '"><div class="col s11"><b>' + result.name.en + '</b></div> <div class="col s1">' + adaresult(result.test.ada)[0] + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Not Italic Test</div> <div class="col s2">' + test(result.test.notitalic) + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Stroke Width Test</div> <div class="col s2">' + test(result.test.stroke) + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Body Width Test</div> <div class="col s2">' + test(result.test.body) + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Letter "I" Ratio</div> <div class="col s2">' + result.letter_i.ratio + '%' + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Letter "H" Ratio</div> <div class="col s2">' + result.letter_h.ratio + '%' + '</div> </div> <div class="collapsible-body row"><span></span> <div class="col s10">Letter "O" Ratio</div> <div class="col s2">' + result.letter_o.ratio + '%' + '</div> </div> </li>';
       } catch (err) {
         console.log("Caught Error!");
         return false;
@@ -57,12 +36,21 @@ function adaresult(result) {
   } else if (result === false) {
     if (document.getElementById("checkbox").checked === false) {
       //return ["✘", "fail"];
-      return ["<i class='material-icons red-text tooltipped' data-position='bottom' data-tooltip='Does not meet ADA Requirements'>cancel</i>", ""];
+      return ["<i class='material-icons red-text tooltipped' data-position='bottom' data-tooltip='Does not meet ADA Requirements'>cancel</i>", ''];
     }
   } else {
     alert("Error! ADA results error.");
   }
 }
+
+function test(testName) {
+  if (testName) {
+    return ["<i class='material-icons green-text'>check_circle</i>"];
+  } else {
+    return ["<i class='material-icons red-text'>cancel</i>"];
+  }
+}
+
 
 function clearFonts() {
   console.log("Clearing Fonts");
